e52bbe0c51751816f4e63c2c206924231c429b8b
Ehsan_Alizadegan <ehsan.alizadegan55@gmail.com>
2024-08-01 20:53:12 +0330

    connect app to the postgres and add typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,29 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+  imports: [
+    //when set name property typeorm can not find any repository and throw this error " Nest can't resolve dependencies of the CoffeesService (?). Please make sure that the argument "CoffeeRepository" at index [0] is available in the CoffeesModule context."
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "postgres",
+      password: "pass1234",
+      database: "postgres",
+      autoLoadEntities: true, 
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(),'src/schema.gql'),
     // buildSchemaOptions : {
     //   numberScalarMode: 'integer',
     // }
-  }), CoffeesModule],
+  }), 
+    CoffeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
